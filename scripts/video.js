b66601d6d522cdcdff298b1f39b6061ffa57c035
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-10-03 03:15:39 +0600

    button functionality added

--- scripts/video.js
+++ scripts/video.js
@@ -6,7 +6,14 @@ function getTimeString(time){
     const minute = parseInt(remainingSecond / 60);
     remainingSecond = remainingSecond % 60;
     return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+};
+const removeActiveClass = () =>{
+    const buttons = document.getElementsByClassName('category-btn');
+    for(let btn of buttons){
+        btn.classList.remove('active');
+    }
 }
+
 // fetch, load and show categories on html
 
 // create loadCategories
@@ -23,6 +30,18 @@ const loadVideos = () => {
         .then(data => displayVideos(data.videos))
         .catch((error) => console.log(error));
 };
+
+const loadCategoryVideos = (id) =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then((res) => res.json())
+        .then(data => {
+            removeActiveClass();
+            const activeButton = document.getElementById(`btn-${id}`);
+            activeButton.classList.add('active');
+            displayVideos(data.category);
+        })
+        .catch((error) => console.log(error));
+}
 
 const cardDemo = {
     "category_id": "1001",
@@ -45,6 +64,20 @@ const cardDemo = {
 
 const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos');
+    videoContainer.innerHTML = "";
+    if(videos.length === 0){
+        videoContainer.classList.remove('grid');
+        videoContainer.innerHTML = `
+        <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+            <img src="assets/Icon.png" />
+            <h2 class="text-center text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+        </div>
+        `;
+        return;
+    }
+    else{
+        videoContainer.classList.add('grid');
+    }
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement('div');
@@ -54,7 +87,7 @@ const displayVideos = (videos) => {
             <img class="h-full w-full object-cover"
             src=${video.thumbnail}
             alt="Shoes" />
-            ${video.others.posted_date?.length === 0 ? '' : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`}
+            ${video.others.posted_date?.length === 0 ? '' : `<span class="absolute text-xs right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`}
         </figure>
         <div class="px-0 py-2 flex gap-2">
             <div>
@@ -80,11 +113,16 @@ const displayCategories = (categories) => {
     const categoryContainer = document.getElementById('categories');
     categories.forEach((item) => {
         // create a button
-        const button = document.createElement('button');
-        button.classList = 'btn';
-        button.innerText = item.category;
+        const buttonContainer = document.createElement('div');
+        // button.classList = 'btn';
+        // button.innerText = item.category;
+        buttonContainer.innerHTML = `
+        <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
+        ${item.category}</button>
+        `
+        
         // add button to category container
-        categoryContainer.append(button);
+        categoryContainer.append(buttonContainer);
     });
 }
 
